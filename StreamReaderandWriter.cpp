a93253c2c162db3ad2597165017824354d2a1d7c
studane <zhidaneprabowoguzlend@gmail.com>
2026-06-01 21:44:15 +0700

    kondisi jika file ada

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -34,3 +34,13 @@ int main()
     //menunjuk ke sebuah file
     infile.open("contohfile.txt");
 
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    //kondisi jika file ada
+    if (infile.is_open())
+    {
+        //perulangan untuk memunculkan setiap baris
+        while (getline(infile, baris)) {
+            cout << baris << endl;
+        }
+        //menutup file jika telah selesai
+        infile.close();
