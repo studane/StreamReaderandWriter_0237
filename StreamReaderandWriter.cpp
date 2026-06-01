bee349cabb1b3367109ccd0043550c84f2bf32a8
studane <zhidaneprabowoguzlend@gmail.com>
2026-06-01 21:44:28 +0700

    kondisi jika file ada

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -44,3 +44,9 @@ int main()
         }
         //menutup file jika telah selesai
         infile.close();
+    }
+    //kondisi jika file tidak ada
+    else cout << "Unable to open file";
+    return 0;
+
+}
