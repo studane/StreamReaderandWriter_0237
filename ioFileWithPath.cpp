0f009a5808ec2a174c773888212f331fc04672c9
studane <zhidaneprabowoguzlend@gmail.com>
2026-06-01 21:32:48 +0700

    menunjuk ke sebuah nama file dengan ekstensi txt

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -14,4 +14,9 @@ int main()
 
     //membuat file dalam mode menulis
     ofstream outfile;
+    //menunjuk ke sebuah nama file dengan ekstensi txt
+    outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
     
