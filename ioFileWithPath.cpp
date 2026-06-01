38d6e30393e8b08414e59259a49338a0ca4eba53
studane <zhidaneprabowoguzlend@gmail.com>
2026-06-01 21:35:58 +0700

    menambah kondisi jika file tidak ada

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -51,3 +51,7 @@ int main()
         //menutup file jika telah selesai di loop
         infile.close();
     }
+    //kondisi jika file tidak ada
+    else cout << "Unable to open file";
+    return 0;
+}
